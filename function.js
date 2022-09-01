80df76d0fe123f0305f68c3f929728fc084cdc4e
Ariel Calipay <83052833+arielcalipay20@users.noreply.github.com>
2022-09-01 11:12:15 +0800

    Add files via upload

---
+++ function.js
@@ -0,0 +1,8 @@
+
+            let showMenu = function() {
+                document.getElementById('link-container').style.right = "0";
+            }
+
+            let hideMenu = function() {
+                document.getElementById('link-container').style.right = "-300px";
+            }
